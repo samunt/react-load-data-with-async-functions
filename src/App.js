1dc5571e8bbe07ac5b0e725e30bb04e194d87392
samunt <sammy.unterman@gmail.com>
2020-06-15 15:25:01 -0400

    async await stuff for api

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import GithubUserInfo from "./GithubUserInfo";
+
+function App() {
+  return (
+    <div className="App">
+      <GithubUserInfo username='samunt'/>
+    </div>
+  );
+}
+
+export default App;
